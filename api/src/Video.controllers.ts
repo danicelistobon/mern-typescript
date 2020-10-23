20b2f31ae111a216a41782e91616c7114f099028
danicelistobon <danielct-97@hotmail.com>
2020-10-23 15:50:10 -0500

    Created video controllers

---
+++ api/src/Video.controllers.ts
@@ -0,0 +1,46 @@
+import { RequestHandler } from 'express';
+import Video from './Video.model';
+
+export const newVideo: RequestHandler = async (req, res) => {
+  const videoFound = await Video.findOne({ url: req.body.url });
+  if (videoFound) {
+    return res.status(303).json({ message: 'The URL already exists!' });
+  }
+
+  const video = new Video(req.body);
+  const savedVideo = await video.save();
+  res.json(savedVideo);
+};
+
+export const getVideos: RequestHandler = async (req, res) => {
+  try {
+    const videos = await Video.find();
+    return res.json(videos);
+  } catch (error) {
+    res.json(error);
+  }
+};
+
+export const getVideo: RequestHandler = async (req, res) => {
+  const videoFound = await Video.findById(req.params.id);
+  if (!videoFound) {
+    return res.status(204).json();
+  }
+  return res.json(videoFound);
+};
+
+export const updateVideo: RequestHandler = async (req, res) => {
+  const videoUpdated = await Video.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  if (!videoUpdated) {
+    return res.status(204).json();
+  }
+  res.json(videoUpdated);
+};
+
+export const deleteVideo: RequestHandler = async (req, res) => {
+  const videoInfo = await Video.findByIdAndDelete(req.params.id);
+  if (!videoInfo) {
+    return res.status(204).json();
+  }
+  return res.json({ message: 'Video deleted', videoInfo });
+};
